d590734cb1570955cf9e5095d0ad29751810aced
shimizu <noon3m.op.sh@gmail.com>
2024-06-23 04:39:05 +0900

    [update]文字数が多い時に対応させた

--- front/src/features/theater/components/agenda/Agenda.tsx
+++ front/src/features/theater/components/agenda/Agenda.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import logo from "../../img/logo.png";
 import "./Agenda.css";
+import { set, words } from "lodash";
 
-export function Agenda() {
+interface Props {
+  title: string;
+}
+
+export const Agenda: React.FC<Props> = ({title}) => {
+
+  const [wordCounter] = useState(title.length);
+  if (wordCounter > 20) {}
+
+  useEffect(() => {
+
+  })
   return (
     <div>
       <div className="header">
         {/* <img src={logo} className="logo" alt="logo" /> */}
       </div>
       <div className="agenda_bg">
-        <p className="agenda_text">決算委員会合同審査会</p>
+        <p className="agenda_text">{title}</p>
       </div>
     </div>
   );
