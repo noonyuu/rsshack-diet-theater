a1d75c2ad1dea3143f6df520900620ad147bc585
michirun <michigami.kosuke@gmail.com>
2024-06-23 06:20:48 +0900

    アニメーション追加

--- front/src/features/theater/components/summary_text/Summary_text.tsx
+++ front/src/features/theater/components/summary_text/Summary_text.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import person from "../../img/person.png";
 import "./Summary_text.css";
+import { Animation } from "../animation/Animation";
 
 export function Summary_text() {
   return (
     <div className="wrapper">
       <img src={person} className="person" alt="person" />
+      <Animation arg={1}></Animation>
       <div className="name_area_bg">
         <p className="name_area">神さくら</p>
       </div>
