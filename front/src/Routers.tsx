48837ed3c26cf2922eb4000dc7160eb5fdb14567
jinjin <sakura0419gen@icloud.com>
2024-06-22 11:30:45 +0900

    [add] ヘッダーとフッター含めて表示もできるようにした [add] Header, Footer新規作成 [add] Home 新規作成

--- front/src/Routers.tsx
+++ front/src/Routers.tsx
@@ -1,9 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Login } from "./features/login/pages/Login";
+import { Home } from "./features/home/pages/Home";
 import { ContextWrapper } from "./context/ContextWrapper";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export const Routers = () => {
   const routesWithoutHeaderAndFooter = [{ path: "/login", element: <Login /> }];
+  const routesWithHeadedrAndFooter = [{ path: "/home", element: <Home /> }]
+
 
   interface LayoutProps {
     element: JSX.Element;
@@ -12,6 +17,17 @@ export const Routers = () => {
   function LayoutWithoutHeaderAndFooter({ element }: LayoutProps) {
     // ヘッダーとフッターなしで要素を表示
     return <>{element}</>;
+  }
+
+  function LayoutWithHeaderAndFooter({ element }: LayoutProps) {
+    // ヘッダーとフッターを含めて要素を表示
+    return (
+      <>
+        <Header />
+        {element}
+        <Footer />
+      </>
+    );
   }
 
   return (
@@ -23,6 +39,14 @@ export const Routers = () => {
             key={path}
             path={path}
             element={<LayoutWithoutHeaderAndFooter element={element} />}
+          />
+        ))}
+        {/* ヘッダーとフッターありで表示したいページ */}
+        {routesWithHeadedrAndFooter.map(({ path, element }) => (
+          <Route
+            key={path}
+            path={path}
+            element={<LayoutWithHeaderAndFooter element={element} />}
           />
         ))}
       </Routes>
