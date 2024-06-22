286cf86ebc22a8e507c9818c892eca544a32bb14
shimizu <noon3m.op.sh@gmail.com>
2024-06-23 04:05:29 +0900

    [update]背景を表示する場所を細かく指定

--- front/src/features/theater/pages/Theater.tsx
+++ front/src/features/theater/pages/Theater.tsx
@@ -4,7 +4,7 @@ import { Summary_text } from "../components/summary_text/Summary_text";
 import { Agenda } from "../components/agenda/Agenda";
 export function Theater() {
   return (
-    <div>
+    <div className="theater-bac">
       <Agenda></Agenda>
       <Summary_text></Summary_text>
     </div>
